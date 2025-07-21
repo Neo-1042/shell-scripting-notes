9c6c2cca50676a918e3a8e4a5025c4c581639bb3
Neo-1042 <rafaelh.71642@gmail.com>
2025-07-20 22:03:39 -0600

    Learn more about WHILE loops, READ from files, BREAK and CONTINUE
    grep xfs /some/file.txt pipe while read LINE

--- 11_WHILE_LOOPS.sh
+++ 11_WHILE_LOOPS.sh
@@ -44,6 +44,7 @@ while [ "$CORRECT" != "y" ] ; do
 done
 echo "END"
 
+===================================================================================================
 # Script performing a BACKUP process
 # Wait until the application server is off the network before it continues the backup.
 
@@ -54,3 +55,50 @@ do
 done
 
 echo "app1 is down, continuing backup."
+===================================================================================================
+# Reading a file, line-by-line
+# The FOR LOOP does NOT work properly here, since it will read word for word.
+# To actually read line-by-line, you have to use a WHILE LOOP
+
+LINE_NUM=1
+
+while read LINE; do
+	echo "${LINE_NUM} : ${LINE}"
+	((LINE_NUM++))
+done < /etc/fstab # Example file
+===================================================================================================
+# Read from the output of a command, instead of a file:
+grep xfs /etc/fstab | while read LINE
+do
+	echo "xfs: ${LINE}"
+done
+===================================================================================================
+# The read command can read from multiple variables:
+
+FS_NUM=1
+grep xfs /etc/fstab | while read FS MP REST # The last variable (REST) contains the rest of the words from that line
+do
+	echo "${FS_NUM}: file system: ${FS}"
+	echo "${FS_NUM}: mount point: ${MP}"
+	((FS_NUM++))
+done
+===================================================================================================
+# BREAK: Exit a LOOP before its normal ending use, but it does not end the script
+
+while true; do
+	read -p "1: Show disk usage. 2: Show uptime." CHOICE
+	case "$CHOICE" in
+		1)
+			df -h # display free disk space, -h = human readable
+			;;
+		2)
+			uptime
+			;;
+		*)
+			break # SUPER IMPORTANT; otherwise, you won't normally exit the while loop
+			;;
+    esac
+done
+===================================================================================================
+# CONTINUE: skip this iteration and continue with the execution of the loop
+
